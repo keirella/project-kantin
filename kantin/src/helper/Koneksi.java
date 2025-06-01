173818d8d78103aa32ec0d11d76f41d79ce9d493
billarizky <salsabillarizki2004@gmail.com>
2025-06-01 23:30:05 +0700

    Update Koneksi.java

--- kantin/src/helper/Koneksi.java
+++ kantin/src/helper/Koneksi.java
@@ -2,26 +2,24 @@ package helper;
 
 import com.mysql.cj.jdbc.MysqlDataSource;
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.SQLException;
+import javax.swing.JOptionPane;
 
 public class Koneksi {
-    static Connection conn;
-
-    public static Connection getConnection() {
-        if (conn == null) {
-            MysqlDataSource dataSource = new MysqlDataSource();
-            dataSource.setDatabaseName("kantin_db");
-            dataSource.setUser("root");
-            dataSource.setPassword("");
-
-            try {
-                conn = dataSource.getConnection();
-                System.out.println("Koneksi Berhasil");
-            } catch (SQLException ex) {
-                ex.printStackTrace();
-                System.err.println("Koneksi Gagal: " + ex.getMessage());
-            }
+    private static MysqlDataSource dataSource;
+    static{
+        dataSource = new MysqlDataSource();
+        dataSource.setURL("jdbc:mysql://localhost:3306/kantin_db");
+        dataSource.setUser("root");
+        dataSource.setPassword("");
+    }
+     public static Connection getConnection() {
+        try {
+            return dataSource.getConnection(); // selalu koneksi baru
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
         }
-        return conn;
     }
 }
